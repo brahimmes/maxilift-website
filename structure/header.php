1031830b0681ba15282ab841a09f4f49308837ec
brahimmes <brahimmes@gmail.com>
2020-01-11 13:43:54 +0100

    push de la configuration initiale

---
+++ structure/header.php
@@ -0,0 +1,13 @@
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="icon" href="assets/images/favicon.ico">
+    <title>Maxilift</title>
+
+    <![endif]-->
+
+
+</head>
---
+++ structure/scripts.php
@@ -0,0 +1,12 @@
+
+<!--[if lt IE 9]>
+<script src="assets/js/ie8-responsive-file-warning.js"></script>
+
+<!-- Bottom scripts (common) -->
+
+
+<![endif]-->
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+
